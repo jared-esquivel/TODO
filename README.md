6a34e7d4e62e1b09056fb01735a9d953c38de358
Leo Villalobos <leo@digitalnest.org>
2024-10-30 14:33:10 -0700

    init: create project structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# Final Project
+
+The final project for the Web Development class at Digital NEST.
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+$message = "Hello, world!";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>My Project</title>
+  <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+    crossorigin="anonymous" />
+  <link rel="stylesheet" href="styles/stylesheet.css" />
+</head>
+
+<body>
+  <h1 class="m-2 p-1">
+    <?php echo $message; ?>
+  </h1>
+  <script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
